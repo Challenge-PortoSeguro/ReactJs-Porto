bbdf618c13de2984348be92ca3d59663352a0a8b
Duh0127 <rodatupmocgamer22@gmail.com>
2023-08-21 10:35:43 -0300

    Criação de arquivo com cores padrões do site

--- src/theme/DefaultColors.js
+++ src/theme/DefaultColors.js
@@ -1,7 +1,8 @@
 const DefaultColors = {
     colors: {
         text: {
-            body: '#e8e8e8',
+            white: '#e8e8e8',
+            black: '#252728',
             description: '#828282',
             placeholder: '#545454',
             onFill: '#e8e8e8',
@@ -18,25 +19,26 @@ const DefaultColors = {
         },
         button: {
             normal: '#00375C',
-            hover: '#7393A8',
+            hover: 'rgba(255, 255, 255, .05)',
             active: '#007AF5',
             disabled: '#67696b',
             secondary: '#e8e8e8',
         },
         background: {
-            primary: '#49B0F5',
-            secondary: '#0084DB',
-            tertiary: '#0065A8',
+            header: '#13293D',
+            body: '#1B98E0',
+            footer: '#13293D',
+            tertiary: '#E8F1F2',
             transparent: 'rgba(0, 0, 0, .30)',
         },
         border: {
-            primary: '#383838',
+            primary: '#8DBEDA',
             field: '#67696b',
             divider: '#949497',
             selected: '#007AF5',
             alert: '#C1292E',
             disabled: '#67696b',
-            transparent: 'rgba(159,173,188,.16)',
+            transparent: 'rgba(255,255,255,.30)',
         },
         status: {
             success: '#00CC55',
