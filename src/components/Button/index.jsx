dd59c434a2dd95bbd11aef01b8804dd57ca913c1
Duh0127 <rodatupmocgamer22@gmail.com>
2023-08-21 10:33:35 -0300

    Componente Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,33 @@
-import React from "react";
-import * as B from "./styles";
+import { useNavigate } from "react-router-dom";
+import * as S from "./styles";
+import PropTypes from "prop-types";
 
-const Button = ({ children, redirect, disabled, variant = 'primary', type = "button", onClick, full}) => {
-    
+function Button({ children, redirect, disabled, variant = 'primary', type = "button", onClick, full }) {
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        if (redirect) {
+            navigate(redirect);
+            return;
+        }
+        if (onClick) onClick();
+    }
+
     return (
-       <B.Button></B.Button>
+        <S.Button type={type} $variant={variant} onClick={handleClick} disabled={disabled} $full={full}>
+            {variant === 'link' ? <S.LinkButton className="Link" to={redirect}>{children}</S.LinkButton> : children}
+        </S.Button>
     );
 }
+
+Button.propTypes = {
+    children: PropTypes.node,
+    redirect: PropTypes.string,
+    variant: PropTypes.oneOf(['primary', 'secondary', 'success', 'danger', 'link']),
+    type: PropTypes.string,
+    onClick: PropTypes.func,
+    disabled: PropTypes.bool,
+    full: PropTypes.bool,
+}
+
 export default Button;
