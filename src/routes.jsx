d23d0cbf7c74b6b0fcf532be39f749aa173f1322
Duh0127 <rodatupmocgamer22@gmail.com>
2023-08-21 10:35:51 -0300

    Arquivo de rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,8 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 
 import Home from "./pages/Home";
-import Login from "./pages/auth/login";
-import Register from "./pages/auth/registro";
+import LoginCliente from "./pages/auth/login/login-cliente";
+import LoginColaborador from "./pages/auth/login/login-colaborador";
+import RegistroCliente from "./pages/auth/registro-cliente";
+import RegistroColaborador from "./pages/auth/registro-colaborador";
 
 const Router = () => {
     return (
@@ -11,8 +13,10 @@ const Router = () => {
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route path="/login/cliente" element={<LoginCliente />} />
+            <Route path="/login/colaborador" element={<LoginColaborador />} />
+            <Route path="/register/cliente" element={<RegistroCliente />} />
+            <Route path="/register/colaborador" element={<RegistroColaborador />} />
             <Route path="*" element={<Error />} />
           </Route>
         </Routes>
