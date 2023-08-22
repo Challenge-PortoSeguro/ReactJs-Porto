dad8834dfddc5261668a85b0030e824b4245e9ca
Duh0127 <rodatupmocgamer22@gmail.com>
2023-08-22 16:52:37 -0300

    New Homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,34 @@
-import react from "react";
+import { useMemo } from "react";
 import * as S from "./styles";
+import illustration from "../../assets/img/illustration14.png";
+import gifAI from "../../assets/img/gif_AI.gif";
+import Button from "../../components/Button";
+import renderIcon from "../../utils/iconGallery";
 
 const Home = () => {
+
+    const icons = useMemo(() => ({
+        play: renderIcon({ name: "play", size: 18, color: "#000" }),
+    }), [])
+
+
     return (
-        <S.WelcomeSection>
-            <S.HomeTitle>Porto Assistent</S.HomeTitle>
-        </S.WelcomeSection>
+        <S.HomeSection>
+            <S.WelcomeDiv>
+                <S.TextDiv>
+                    <S.HomeTitle>Bem vindo à revolucionaria asseguradora de veículos pesados</S.HomeTitle>
+                    <S.HomeSubtitle>Descubra a revolução utilizando IA avançada para a coleta de dados precisos, garantindo proteção incomparável e processos eficientes.</S.HomeSubtitle>
+                    <S.ButtonsDiv>
+                        <Button variant="primary" redirect="/register/cliente">Cadastre-se</Button>
+                        <Button variant="secondary" link="https://youtu.be/dQw4w9WgXcQ">{icons.play} Assistir Vídeo</Button>
+                    </S.ButtonsDiv>
+                </S.TextDiv>
+                <S.Illustration src={gifAI} alt=""/>
+            </S.WelcomeDiv>
+            <S.WhoAreWeDiv>
+                <S.WhoAreWeTitle>Quem somos</S.WhoAreWeTitle>
+            </S.WhoAreWeDiv>
+        </S.HomeSection>
     );
 }
 export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,15 +1,111 @@
 import styled from 'styled-components';
+import fundo from '../../assets/img/background.jpg';
 
-export const WelcomeSection = styled.section`
-    background-color: ${({ theme }) => theme.colors.background.body};
+export const HomeSection = styled.section`
+    background-color: ${({ theme }) => theme.colors.background.tertiary};
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    height: 100%;
+    height: fit-content;
 `;
 
+export const WelcomeDiv = styled.div`
+    background-color: ${({ theme }) => theme.colors.background.white};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    width: 100%;
+
+    @media (max-width: 830px) {
+        flex-direction: column-reverse;
+        justify-content: start;
+    }
+`;
+export const TextDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    width: 50%;
+    height: 100%;
+    padding: 0 4.5rem;
+
+    @media (max-width: 1024px) {
+        padding: 0 2rem;
+    }
+    @media (max-width: 830px) {
+        width: 100%;
+        height: fit-content;
+        padding: 0 4rem;
+    }
+    @media (max-width: 425px) {
+        padding: 0 2rem;
+    }
+`;
 export const HomeTitle = styled.h1`
+    color: ${({ theme }) => theme.colors.text.black};
+    font-size: 3.5rem;
+    font-weight: 600;
+    margin-bottom: 1rem;
+    font-family: "Inter", sans-serif;
+
+    @media (max-width: 1024px) {
+        font-size: 2.5rem;
+    }
+    @media (max-width: 830px) {
+        text-align: center;
+    }
+    @media (max-width: 425px) {
+        font-size: 1.5rem;
+    }
+`;
+export const HomeSubtitle = styled.h2`
+    color: ${({ theme }) => theme.colors.text.black};
+    font-size: 1.3rem;
+    font-weight: 400;
+    font-family: "Inter", sans-serif;
+
+    @media (max-width: 1024px) {
+        font-size: 1rem;
+    }
+`;
+export const ButtonsDiv = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    width: 100%;
+    gap: 1rem;
+
+    @media (max-width: 830px) {
+        justify-content: center;
+    }
+`;
+export const Illustration = styled.img`
+    object-fit: contain;
+    object-position: center;
+    width: 50%;
+
+    @media (max-width: 830px) {
+        width: 60%;
+    }
+`;
+
+
+
+export const WhoAreWeDiv = styled.section`
+    background-color: ${({ theme }) => theme.colors.background.primary};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 80%;
+    border-radius: 8px;
+`;
+
+export const WhoAreWeTitle = styled.h1`
     color: ${({ theme }) => theme.colors.text.primary};
     font-size: 2.5rem;
     font-weight: 700;
