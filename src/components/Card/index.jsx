cb84261593234073c573bb4a55cd48739c8178f3
Duh0127 <rodatupmocgamer22@gmail.com>
2023-08-27 19:10:55 -0300

    Adicionando mais props ao component Card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,7 +4,7 @@ import renderIcon from "../../utils/iconGallery";
 import Button from "../Button";
 import { redirect } from "react-router-dom";
 
-function Card({ title, desc, img, alt, icon = "user", redirect, buttonLink1, buttonLink2, iconButton1 = "user", iconButton2 = "user" }) {
+function Card({ title, desc, img, alt, icon = "user", redirect1, redirect2, buttonLink1, buttonLink2, iconButton1 = "user", iconButton2 = "user" }) {
   const Icon = useMemo(() => renderIcon({ name: icon, color: "#fff", size: 104 }), [icon]);
   const IconButton1 = useMemo(() => renderIcon({ name: iconButton1, color: "#fff", size: 32 }), []);
   const IconButton2 = useMemo(() => renderIcon({ name: iconButton2, color: "#fff", size: 32 }), []);
@@ -26,8 +26,8 @@ function Card({ title, desc, img, alt, icon = "user", redirect, buttonLink1, but
         <C.CardTitle>{title}</C.CardTitle>
         <C.CardSubtitle>{desc}</C.CardSubtitle>
         <C.CardFooter>
-          {buttonLink1 || redirect ? <Button variant="link" redirect={redirect} link={buttonLink1}>{IconButton1}</Button> : null}
-          {buttonLink2 || redirect ? <Button variant="link" redirect={redirect} link={buttonLink2}>{IconButton2}</Button> : null}
+          {buttonLink1 || redirect1 ? <Button variant="link" redirect={redirect1} link={buttonLink1}>{IconButton1}</Button> : null}
+          {buttonLink2 || redirect2 ? <Button variant="link" redirect={redirect1} link={buttonLink2}>{IconButton2}</Button> : null}
         </C.CardFooter>
       </C.CardBody>
     </C.CardContainer>
