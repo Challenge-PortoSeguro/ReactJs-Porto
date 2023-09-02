1b7cfc722cf1d587adae33520342847819df6842
matteocortezi <matteocortezi1@gmail.com>
2023-09-02 00:13:00 -0300

    Alterando função de pegar data do footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,13 @@
 import * as S from "./styles";
 
 const Footer = () => {
+
+    const date = new Date();
+    const year = date.getFullYear();
+
     return (
         <S.Footer>
-            <S.Text>Porto Assistent © 2023 - Todos os direitos reservados</S.Text>
+            <S.Text>Porto Assistant © {year} - Todos os direitos reservados</S.Text>
         </S.Footer>
     );
 };
